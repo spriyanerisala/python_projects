94085479b5e36b8877b5ce802d57dbb1305aae3e
siva priya <nspriya005@gmail.com>
2026-04-27 22:27:36 +0530

    app.py updated

--- projects/predict_your_marks/server/app.py
+++ projects/predict_your_marks/server/app.py
@@ -5,18 +5,23 @@ from routes.student import student_bp
 from config.db import check_connection
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
 
 app = Flask(__name__)
 check_connection()
 
-CORS(app, origins=[os.getenv("FRONTEND_URL")])
+frontend_url = os.getenv("FRONTEND_URL", "https://python-projects-e6vh.vercel.app")
+print(f"Allowing CORS for: {frontend_url}")  # helpful for debugging in Render logs
+
+CORS(app, origins=[frontend_url])
 
 app.register_blueprint(predict_bp)
 app.register_blueprint(student_bp)
 
 @app.route('/')
 def home():
-    return {"message":"Flask API is running"}
+    return {"message": "Flask API is running"}
+
 if __name__ == '__main__':
     app.run(debug=True)
